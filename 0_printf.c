41e2f206419cfa3d15d4762615964175df16d0fe
saraa93-bit <saraa93177228@gmail.com>
2023-10-17 04:37:16 -0700

    yt

--- 0_printf.c
+++ 0_printf.c
@@ -2,31 +2,34 @@
 
 /**
 * _printf - Function that produces output according to a format
-* @format:  A pointer parameter to input format
+* @format: A pointer parameter to input format
 *
 * Return: Number of printed characters.
 */
 
 int _printf(const char *format, ...)
 {
+	myFormat myFormatChecks[] = {
+		{"%c", print_char},
+		{"%s", print_string},
+		{"%d", printInteger},
+		{"%i", printInteger},
+		{"%%", percent_print}
+		{"%r", print_rev_str}
+	};
 	int i = 0, j = 0;
 	va_list listedPrint;
-	myFormat myFormatChecks[] = {
-		{"c", print_char},
-		{"s", print_string},
-		{"d", printInteger},
-		{"i", printInteger}
-	};
-	if (format == NULL || format[0] == '\0')
+
+	va_start(listedPrint, format);
+	if (format[i] == NULL || format[0] == '%' && format[1] == '\0')
 	{
 		return (-1);
 	}
-	va_start(listedPrint, format);
 
 	while (format[i] != '\0')
 	{
 		j = 0;
-		while (j < 4)
+		while (j < 6)
 		{
 			if (format[i] == '%' && format[i + 1] != '\0')
 			{
--- main.h
+++ main.h
@@ -19,12 +19,13 @@ typedef struct myFormatCheck
 {
         char *spsifier;
         int (*printIt)(va_list);
-} myFormat;
+}myFormat;
 
 int _printf(const char *format, ...); /* Handle various format specifiers */
 int print_char(va_list listedPrint); /* Handle format of character */
 int print_string(va_list listedPrint); /* Handle format of string */
 int printInteger(va_list listedPrint); /* Handle format of integer */
 int _putchar(char c); /* print a charectar to output*/
+int print_rev_str(va_list listedPrint); /* Handle format of reverse string */
 
 #endif 
