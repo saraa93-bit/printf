5bff34fa492b81a382fae5b35d0ab9e471d98d67
sabah-hue <sabah.abdelbaset@gmail.com>
2023-10-18 01:54:49 +0300

    debug

--- print_rev_str.c
+++ print_rev_str.c
@@ -9,16 +9,21 @@
 
 int print_rev_str(va_list listedPrint)
 {
-	int x, n = x - 1;
+	int x, n;
 	char *str = va_arg(listedPrint, char *);
 
 	if (!str)
 		return (-1);
 	for (x = 0; str[x] != '\0'; x++)
+	{
+	}
+	n = x - 1;
 
-	while (n >= 0);
-		_putchar(str[n];
+	while (n >= 0)
+	{
+		_putchar(str[n]);
 		n--;
+	}
 return (x);
 }
 
