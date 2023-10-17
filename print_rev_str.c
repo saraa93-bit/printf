e23b0192e656a22d40ed417eed4ed5e9b10615e6
saraa93-bit <saraa93177228@gmail.com>
2023-10-17 14:36:23 -0700

    upto

---
+++ print_rev_str.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+* print_rev_str - afunction prints the reversed string
+* @listedPrint: list of arguments will be printed
+*
+* Return: the string
+*/
+
+int print_rev_str(va_list listedPrint)
+{
+	int x, n = x - 1;
+	char *str = va_arg(listedPrint, char *);
+
+	if (!str)
+		return (-1);
+	for (x = 0; str[x] != '\0'; x++)
+
+	while (n >= 0);
+		_putchar(str[n];
+		n--;
+return (x);
+}
+
