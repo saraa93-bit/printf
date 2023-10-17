3c6b5b085a0e8e669365c80c569716c09e7f0359
saraa93-bit <saraa93177228@gmail.com>
2023-10-17 00:07:16 -0700

    commit

--- print_char.c
+++ print_char.c
@@ -3,7 +3,7 @@
 /**
 * print_string - a function that Prints a string
 *
-* @args: list of arguments will be printed
+* @listedprint: list of arguments will be printed
 * Return: number of printed argument, -1 if nothing printed
 */
 
@@ -28,7 +28,7 @@ int print_string(va_list listedPrint)
 * print_char - a function that Prints a charectar
 *
 * Description: Prints a  charectar.
-* @args: the arguments list
+* @listedprint: the arguments list
 * Return: number of characters printed.
 */
 
